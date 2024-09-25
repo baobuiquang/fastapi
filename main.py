13c61816a550a046dca7e2e70b5f0225f5f1d83d
Bao Bui-Quang <quangbao.dev@gmail.com>
2024-09-25 14:06:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 import uvicorn
 import _config
@@ -14,6 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# ENDPOINT: /api
 # Method 1: Can handle string contains "/" character
 # > http://localhost:8888/path_1/?input_text_1=TYPE_INPUT_HERE_1
 @app.get("/path_1/")
@@ -22,6 +24,7 @@ def booooo_1(input_text_1: str = "DEFAULT_TEXT_1"):
         "message_1": f"Hello {input_text_1}!"
     }
 
+# ENDPOINT: /api
 # Method 2: Shorter URL, variable name not matter
 # > http://localhost:8888/path_2/TYPE_INPUT_HERE_2
 @app.get("/path_2/{input_text_2}")
@@ -29,6 +32,10 @@ def booooo_2(input_text_2: str = "DEFAULT_TEXT_2"):
     return {
         "message_2": f"Hello {input_text_2}!"
     }
+
+# ENDPOINT: /app
+# > http://localhost:8888/app
+app.mount("/app", StaticFiles(directory="app", html=True), name="app")
 
 if _config.IS_DEVELOPING == False:
     uvicorn.run(
